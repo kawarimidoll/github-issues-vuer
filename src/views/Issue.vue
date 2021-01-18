66a2a930ea07a8e449c75128f9fbdb9e6038da9a
kawarimidoll <kawarimidoll+git@gmail.com>
2021-01-18 16:54:34 +0900

    use fetch api instead of dummy data

--- src/views/Issue.vue
+++ src/views/Issue.vue
@@ -14,18 +14,7 @@
 </template>
 
 <script>
-const issues = [
-  { id: 1, number: 1, title: "issue title 1", body: "issue body 1" },
-  { id: 2, number: 2, title: "issue title 2", body: "issue body 2" },
-  { id: 3, number: 3, title: "issue title 3", body: "issue body 3" },
-  { id: 4, number: 4, title: "issue title 4", body: "issue body 4" },
-  { id: 5, number: 5, title: "issue title 5", body: "issue body 5" },
-  { id: 6, number: 6, title: "issue title 6", body: "issue body 6" },
-  { id: 7, number: 7, title: "issue title 7", body: "issue body 7" },
-  { id: 8, number: 8, title: "issue title 8", body: "issue body 8" },
-  { id: 9, number: 9, title: "issue title 9", body: "issue body 9" },
-  { id: 10, number: 10, title: "issue title 10", body: "issue body 10" },
-];
+const API_URL = "http://api.github.com/repos/facebook/react/issues/";
 
 export default {
   props: ["number"],
@@ -50,8 +39,23 @@ export default {
   methods: {
     fetchData() {
       this.loading = true;
-      this.issue = issues[Number(this.number)];
-      this.loading = false;
+      fetch(`${API_URL}${this.number}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+          this.issue = json;
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.warn(err);
+          this.issue = {};
+          this.loading = false;
+        });
     },
   },
 };
--- src/views/Issues.vue
+++ src/views/Issues.vue
@@ -19,10 +19,8 @@
 </template>
 
 <script>
-// [Vue 3 Fetch Data and Consume REST APIs | Techiediaries](https://www.techiediaries.com/vue-3-fetch-data-rest-api/)
-// [Vue3 Composition API でのVue Routerの使い方 - Qiita](https://qiita.com/azukiazusa/items/9f467fdea7298baf3476)
 const API_URL =
-  "http://api.github.com/repos/facebook/react/issues?per=10&page=";
+  "http://api.github.com/repos/facebook/react/issues?per_page=10&page=";
 
 export default {
   data: function () {
@@ -47,21 +45,23 @@ export default {
   methods: {
     fetchData() {
       this.loading = true;
-      this.page = this.$route.query.page || "1";
-      console.log("fetch!", `${API_URL}${this.page}`);
-      this.issues = [
-        { id: 1, number: 1, title: "issue title 1", body: "issue body 1" },
-        { id: 2, number: 2, title: "issue title 2", body: "issue body 2" },
-        { id: 3, number: 3, title: "issue title 3", body: "issue body 3" },
-        { id: 4, number: 4, title: "issue title 4", body: "issue body 4" },
-        { id: 5, number: 5, title: "issue title 5", body: "issue body 5" },
-        { id: 6, number: 6, title: "issue title 6", body: "issue body 6" },
-        { id: 7, number: 7, title: "issue title 7", body: "issue body 7" },
-        { id: 8, number: 8, title: "issue title 8", body: "issue body 8" },
-        { id: 9, number: 9, title: "issue title 9", body: "issue body 9" },
-        { id: 10, number: 10, title: "issue title 10", body: "issue body 10" },
-      ];
-      this.loading = false;
+      this.page = this.$route.query?.page || "1";
+      fetch(`${API_URL}${this.page}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+        })
+        .then((json) => {
+          this.issues = json;
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.warn(err);
+          this.issues = [];
+          this.loading = false;
+        });
     },
   },
 };
