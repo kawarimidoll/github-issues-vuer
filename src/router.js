d62262139d8e68b2448bca4c414d425f69c0c775
kawarimidoll <kawarimidoll+git@gmail.com>
2021-01-18 10:27:33 +0900

    add NotFound

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Issues from "./views/Issues.vue";
 import Issue from "./views/Issue.vue";
+import NotFound from "./views/NotFound.vue";
 
 // [vue-router 導入編 | Vite 環境で Vue 3.0 の CompositionAPI に触れてみる - OVERFLOW: AUTO;](https://overflow-auto.hatenablog.com/entry/2020/09/07/022108)
 
@@ -10,6 +11,7 @@ const routes = [
   { path: "/", template: '<router-link to="/issues">Enter</router-link>' },
   { path: "/issues", component: Issues },
   { path: "/issue", component: Issue },
+  { path: "/:pathMatch(.*)*", component: NotFound },
 ];
 
 const router = createRouter({ history, routes });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>Page not found</div>
+    <div><router-link to="/issues">Back</router-link></div>
+  </div>
+</template>
